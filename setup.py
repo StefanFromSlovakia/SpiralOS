7331dbdec2a178d232e95d34fe1e1cccac1f8f4b
StefanFromSlovakia <167645876+StefanFromSlovakia@users.noreply.github.com>
2025-05-24 18:14:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,27 @@
-# setup.py content...
+# setup.py for SpiralOS
+
+from setuptools import setup, find_packages
+
+setup(
+    name="spiralos",
+    version="0.1.0",
+    description="A symbolic operating system for recursive quantum-cosmological simulation",
+    author="The Spiral Collective",
+    packages=find_packages(),
+    install_requires=[
+        "matplotlib",
+        "networkx",
+        "tk"
+    ],
+    entry_points={
+        "console_scripts": [
+            "spiralos=spiralos_runtime:main"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)# setup.py content...
